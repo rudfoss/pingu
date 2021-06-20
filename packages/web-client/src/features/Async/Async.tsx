ce12c986173543cc17ec973abe89c922d09a3c53
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2021-06-20 16:38:24 +0200

    Added Async loading test

---
+++ packages/web-client/src/features/Async/Async.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Async = () => <h1>This component exists in a separate bundle</h1>
+
+export default Async
--- packages/web-client/src/router/AppRouter.tsx
+++ packages/web-client/src/router/AppRouter.tsx
@@ -1,28 +1,35 @@
-import React from "react"
+import React, { Suspense, lazy } from "react"
 import Env from "pages/Env"
 import Home from "pages/Home"
 import { PostRouter } from "pages/Post"
 import { Route, Switch } from "react-router"
 
-export const AppRouter = () => (
-	<Switch>
-		<Route path="/post/:postId">
-			<PostRouter />
-		</Route>
-		<Route path="/post">
-			<PostRouter />
-		</Route>
-		<Route path="/env">
-			<Env />
-		</Route>
-		<Route path="/" exact>
-			<Home />
-		</Route>
+const Async = lazy(() => import("features/Async"))
 
-		<Route>
-			<h1>Not found</h1>
-		</Route>
-	</Switch>
+export const AppRouter = () => (
+	<React.Suspense fallback={<p>Loading...</p>}>
+		<Switch>
+			<Route path="/post/:postId">
+				<PostRouter />
+			</Route>
+			<Route path="/post">
+				<PostRouter />
+			</Route>
+			<Route path="/env">
+				<Env />
+			</Route>
+			<Route path="/async">
+				<Async />
+			</Route>
+			<Route path="/" exact>
+				<Home />
+			</Route>
+
+			<Route>
+				<h1>Not found</h1>
+			</Route>
+		</Switch>
+	</React.Suspense>
 )
 
 export default AppRouter
